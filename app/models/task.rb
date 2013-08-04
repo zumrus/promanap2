42cbb0c86dc49b3bf4fdda20339dbc86757b46b0
Marat Khamadeev <Marat.Khamadeev@gmail.com>
2013-08-04 12:44:57 +0400

    Сделал так, чтобы статус тасков отображался как Done&To do вместо булевых значений

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ActiveRecord::Base
   attr_accessible :content, :name, :status, :project_id
   belongs_to :project
+  
+  def donetodo
+    if self.status?
+      "Done"
+    else
+      "To do"
+    end
+  end
 end
